557ba0788fba6436ecec34b751af02201a94323e
vail <ysywix@gmail.com>
2024-07-15 22:08:31 +0400

    feat: update rules, stylelint v16

--- index.js
+++ index.js
@@ -1,49 +1,14 @@
-module.exports = {
+export default {
   extends: [
     'stylelint-config-recommended-scss',
     'stylelint-config-standard',
-    'stylelint-config-rational-order-fix',
-    'stylelint-config-prettier',
+    '@sazuru/stylelint-config-rational-order',
   ],
   plugins: ['@up-line/stylelint-bem-ruleset'],
   rules: {
+    'import-notation': null,
     /** Enable BEM rules */
     'stylelint-bem-ruleset/classnames': true,
     'stylelint-bem-ruleset/nested': true,
-
-    /** That rules are needed to make empty line between CSS blocks
-     * (due to prettier kill that)
-     * */
-    'at-rule-empty-line-before': [
-      'always',
-      {
-        except: ['blockless-after-same-name-blockless'],
-        ignore: ['after-comment', 'first-nested'],
-        severity: 'warning',
-      },
-    ],
-    'custom-property-empty-line-before': [
-      'always',
-      {
-        except: ['after-custom-property'],
-        ignore: ['after-comment', 'first-nested', 'inside-single-line-block'],
-        severity: 'warning',
-      },
-    ],
-    'declaration-empty-line-before': [
-      'always',
-      {
-        except: ['after-declaration'],
-        ignore: ['after-comment', 'first-nested', 'inside-single-line-block'],
-        severity: 'warning',
-      },
-    ],
-    'rule-empty-line-before': [
-      'always',
-      {
-        ignore: ['after-comment', 'first-nested'],
-        severity: 'warning',
-      },
-    ],
   },
-}
+};
